6033eafdb460ddf0ed3c153dc47ebaa950fae2cc
Saul Ramirez <saulrmzlpz@outlook.com>
2022-10-24 00:13:58 -0500

    server operando con flutter.

--- sockets/socket.js
+++ sockets/socket.js
@@ -10,4 +10,12 @@ io.on('connection', client => {
         console.log('Mensajeee!!', payload);
         io.emit('mensaje', {admin: 'Mensaje de retorno'});
     });
-  });
+
+    client.on('emitir-mensaje',( payload) =>{
+        // io.emit('nuevo-mensaje', payload); //Esto emite mensaje a todos
+        client.broadcast.emit('nuevo-mensaje', payload);  //Esto emite mensaje a todos menos al cliente de origen 
+        console.log(payload);}
+    );
+});
+
+
