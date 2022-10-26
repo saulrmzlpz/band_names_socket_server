12374ef0a7dbbed61811dbd9eca82442551cea74
Saul Ramirez <saulrmzlpz@outlook.com>
2022-10-26 02:12:51 -0500

    Fin sección 4

--- sockets/socket.js
+++ sockets/socket.js
@@ -1,7 +1,23 @@
-const { io } = require('../index')
+const { io } = require('../index');
+const Band = require('../models/band');
+const Bands = require('../models/bands');
+
+
+const bands = new Bands();
+
+bands.addBand(new Band('Queen'));
+bands.addBand(new Band('Bon Jovi'));
+bands.addBand(new Band('Heroes del Silencio'));
+bands.addBand(new Band('Metallica'));
+
+console.log(bands)
+
 //Mensajes de sockets
 io.on('connection', client => {
     console.log('cliente conectado');
+
+    client.emit('active-bands', bands.getBands());
+
     client.on('disconnect', () => {
         console.log('cliente desconectado');
     });
@@ -11,11 +27,29 @@ io.on('connection', client => {
         io.emit('mensaje', {admin: 'Mensaje de retorno'});
     });
 
-    client.on('emitir-mensaje',( payload) =>{
-        // io.emit('nuevo-mensaje', payload); //Esto emite mensaje a todos
-        client.broadcast.emit('nuevo-mensaje', payload);  //Esto emite mensaje a todos menos al cliente de origen 
-        console.log(payload);}
-    );
+    client.on('vote-band',function( payload) {
+        bands.voteBand(payload.id);
+        io.emit('active-bands', bands.getBands());
+
+    });
+
+    client.on('add-band',function( payload) {
+        bands.addBand(new Band(payload.name));
+        io.emit('active-bands', bands.getBands());
+
+    });
+
+    client.on('delete-band',function( payload) {
+        bands.deleteBand(payload.id);
+        io.emit('active-bands', bands.getBands());
+
+    });
+
+    // client.on('emitir-mensaje',( payload) =>{
+    // io.emit('nuevo-mensaje', payload); //Esto emite mensaje a todos
+    //     client.broadcast.emit('nuevo-mensaje', payload);  //Esto emite mensaje a todos menos al cliente de origen 
+    //     console.log(payload);}
+    // );
 });
 
 
